2c9e70821d7823592ca548dc1ac58de88de192cb
Amit sharma <sharmaamit9627172414@gmail.com>
2025-07-12 00:05:50 +0530

    fix

--- Server/routes/adminDonorRoutes.js
+++ Server/routes/adminDonorRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 
-const { getAllDonors } = require("../controllers/admindonorController");
+const { getAllDonors } = require("../controllers/adminDonorController");
 const { authMiddleware, verifyAdmin } = require("../middleware/authMiddleware");
 
 router.get("/donors", authMiddleware, verifyAdmin, getAllDonors);
