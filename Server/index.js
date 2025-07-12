8a0b6a1ef74ff6f0332cdb64c62538f6c6597370
Amit sharma <sharmaamit9627172414@gmail.com>
2025-07-12 10:47:27 +0530

    commit

--- Server/index.js
+++ Server/index.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://wastetoplatefooddonation.vercel.app/",
+    origin: "https://wastetoplatefooddonation.vercel.app",
     credentials: true,
   })
 );
